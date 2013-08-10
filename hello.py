70b6fb6856cdd791bf8d678c3d967f7b7f63a08b
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-08-10 21:15:00 +0100

    Greet subdomains instead of the world

--- hello.py
+++ hello.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_envvar('ENDOFIT_SETTINGS')
 
-@app.route('/')
-def hello_world():
-    return "Hello world!"
+@app.route('/', subdomain='<page_name>')
+def hello_world(page_name):
+    return "Hello {page_name}".format(**{'page_name': page_name})
 
 if __name__ == '__main__':
     app.run()
