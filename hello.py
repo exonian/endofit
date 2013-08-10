230150a41c498601beec6310838a2655bbb9ebcb
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2013-08-10 23:36:45 +0100

    Start using templates

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 app.config.from_envvar('ENDOFIT_SETTINGS')
 
 @app.route('/', subdomain='<page_name>')
 def hello_world(page_name):
-    return "Hello {page_name}".format(**{'page_name': page_name})
+    return render_template('hello.html', page_name=page_name)
 
 if __name__ == '__main__':
     app.run()
